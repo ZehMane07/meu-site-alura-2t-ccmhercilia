23d67b9100804d62e807b3ba4939912ca95d1456
ZehMane07 <105722115+ZehMane07@users.noreply.github.com>
2024-08-23 10:24:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Criar uma página HTML estilizada com CSS
+// essa página tem que possuir
+// - uma imagem de fundo
+// - um botão
+// A Imagem deve ocupar 100% da tela
+// O botaão deve estar centralizado no meio da TELA
+// O botão deve ter uma cor aplicada
+// O texto interno do botão poderá ser pessoal.
+
+
+// VOU NADA
